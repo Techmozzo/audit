5cfbbef54b0d32d039bf23424367a4aa3ff1086f
inyaJohnson <intoajohnson@gmail.com>
2021-09-12 22:30:45 +0100

    Add appspec.yml

--- deployment/scripts/after_install.sh
+++ deployment/scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Set permissions to storage and bootstrap cache
-#sudo chmod -R 0777 /var/www/html/storage
-#sudo chmod -R 0777 /var/www/html/bootstrap/cache
+sudo chmod -R 0777 /var/www/html/storage
+sudo chmod -R 0777 /var/www/html/bootstrap/cache
 #
 #cd /var/www/html
 #
